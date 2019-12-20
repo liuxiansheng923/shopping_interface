8a5e22edc2701edc0c67f3c5e32d2f5ff8f6d15c
liuxiansheng923 <mengshuai923@163.com>
2019-12-20 12:16:46 +0800

    modify

---
+++ js/javascript.js
@@ -0,0 +1,86 @@
+$(function(){
+    $('button').click(function(){
+        var index = $(this).index();
+        var num;
+        var price = $(this).siblings('.one_price').val();
+        
+        // console.log(index);
+        if(index == 1){
+            if($(this).siblings('.count').val()>=1){
+                num = $(this).siblings('.count').val();
+                num--;
+                $(this).siblings('.count').val(num);
+                num = num*price;
+                $(this).siblings('.money').val(num.toFixed(2));
+            }
+            
+        }
+            
+        else{
+            num = $(this).siblings('.count').val();
+            num++;
+            $(this).siblings('.count').val(num);
+            num = num*price;
+            $(this).siblings('.money').val(num.toFixed(2));
+        }
+
+        getSum();
+
+    });
+
+    // 当商品数量发生变化时
+    $('.count').change(function(){
+        var price = $(this).siblings('.one_price').val();
+        var num = $(this).val();
+        num = num*price;
+        $(this).siblings('.money').val(num.toFixed(2));
+    });
+    
+    getSum();
+
+    $('.count').change(getSum);
+    // 全选和全不选
+    $('.checkAll').change(function(){
+        console.log($(this).prop('checked'));
+        $('.s-check').prop('checked',$(this).prop('checked'));
+        
+        
+    });
+    $('.s-check').change(function(){
+            // console.log($(".s-check:checked").length);
+            if($(".s-check:checked").length == 3)
+                $('.checkAll').prop('checked',true);
+            else
+                $('.checkAll').prop('checked',false);
+        });
+
+    // 删除事件
+        // 删除当前商品
+    $('.del').click(function(){
+        $(this).parents('.item').remove();
+        getSum();
+    });
+        // 删除复选框选中的商品
+    $('.checked_del a').click(function(){
+        $('.s-check:checked').parents('.item').remove();
+        getSum();
+    });
+        //  全部删除
+    $('.all_del').click(function(){
+        $('.item').remove();
+        getSum();
+    });
+    // 封装函数：求总件数和、总计
+    function getSum(){
+        var count = 0;
+        var money = 0;
+        $('.count').each(function(index,Ele){
+            count+=parseInt($(Ele).val());
+        });
+        $('.count_all').val(count);
+        $('.money').each(function(index,Ele){
+            money+=parseFloat($(Ele).val());  
+        });
+        $('.money_all').val(money.toFixed(2));
+    }
+})
